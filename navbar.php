7dcbc61e7efa501a43dd70d15cb2ac0af3fec901
redpanda02 <162645666+redpanda02@users.noreply.github.com>
2025-03-10 21:38:43 +0000

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,27 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Gestion Smarttech</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php">Gestion des Employés</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="index_client.php">Gestion des Clients</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Autres
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="autres_page1.php">Page 1</a></li>
+                        <li><a class="dropdown-item" href="autres_page2.php">Page 2</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
